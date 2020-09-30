ce0b834591be94098cd49eca33291e70d59d3a17
Sergey Tarasevich <70640645+Sergey-Tarasevich@users.noreply.github.com>
2020-09-30 15:21:53 +0300

    feat: add solution task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-test
 exports.min = function min(array) {
-    return 0;
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    return Math.min(...array)
 }
 
 exports.max = function max(array) {
-    return 0;
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    return Math.max(...array)
 }
 
 exports.avg = function avg(array) {
-    return 0;
+    if (!array || array.length == 0) {
+        return 0;
+    }
+    let num = 0
+    for (let i = 0; i < array.length; i++) {
+        num += array[i]
+    }
+    return (num / array.length)
 }
